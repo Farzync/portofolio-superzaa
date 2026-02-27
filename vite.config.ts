040967aeedf25ad7df257b8aefb9dbc7cea43287
Faeza Raziq <122501938+Farzync@users.noreply.github.com>
2026-02-27 23:31:56 +0700

    fix(vite): change config to static object for wrangler compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
@@ -11,10 +10,10 @@ export default defineConfig(() => ({
       overlay: false,
     },
   },
-  plugins: [react()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})
